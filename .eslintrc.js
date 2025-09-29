496548dc265cdf4feb87e8583d47521e6824898b
martyall <martyall@protonmail.com>
2025-09-28 19:22:53 -0700

    feat: configure ESLint with TypeScript rules and best practices
    Successfully completed "Create ESLint configuration file" - 1 requirements addressed

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    '@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+  rules: {
+    // TypeScript-specific rules
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+
+    // General ESLint rules
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+
+    // Best practices
+    'eqeqeq': 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-new-func': 'error',
+    'no-new-wrappers': 'error',
+  },
+};
